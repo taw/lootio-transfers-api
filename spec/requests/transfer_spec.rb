8fc10a3d061009dce6d4a06353568eab3eeedbec
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-10-30 04:01:58 +0000

    Rails app in API mode

---
+++ spec/requests/transfer_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Transfer requests", type: :request do
+  it "POST /users/:user_id/transfers" do
+
+  end
+
+  it "DELETE /users/:user_id/transfers/:id" do
+
+  end
+
+  it "DELETE /users/:user_id/transfers/:id" do
+
+  end
+
+  it "PUT /users/:user_id/transfers/:id" do
+
+  end
+end
---
+++ spec/requests/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "User requests", type: :request do
+  it "POST /users" do
+
+  end
+
+  it "DELETE /users/:id" do
+
+  end
+
+  it "DELETE /users/:id" do
+
+  end
+
+  it "PUT /users/:id" do
+    
+  end
+end
